a3e963ae31714bab8d0bcb4304f0aa50e42202c5
James Golding <69616595+JamesGolding-myob@users.noreply.github.com>
2020-12-08 14:25:24 +1100

    initial project setup

---
+++ Conways.Game.Of.Life.Code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Conways.Game.Of.Life.Code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
