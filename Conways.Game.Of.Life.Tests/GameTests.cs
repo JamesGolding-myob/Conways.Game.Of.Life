e3a2ef58437961aee3b2b55fd7b2c6517d334185
James Golding <69616595+JamesGolding-myob@users.noreply.github.com>
2021-01-07 13:53:51 +1100

    updated GameTest

--- Conways.Game.Of.Life.Tests/GameTests.cs
+++ Conways.Game.Of.Life.Tests/GameTests.cs
@@ -9,24 +9,25 @@ namespace Conways.Game.Of.Life
         StubUI ui = new StubUI(); 
 
         [Fact]
-        public void UserSetsTheTopRowforTheGridToBeAliveAndSeesThatPatternDisplayedToThem()
+        public void UserInitiallyAetsTopRowAliveAllCellsSurviveToNextGeneration()
         {
-             var expectedOutput =   " . " + " . " + " . " + " A \n" +
+             var expectedOutput =   " A " + " A " + " A " + " A \n" +
                                     " . " + " . " + " . " + " . \n" +
                                     " . " + " . " + " . " + " . \n";
             
  
             Game game = new Game(ui, displayFormatter, inputConverter);
             ui.AddToQueue("3,4");
-            ui.AddToQueue("2,0 2,2 2,3");
+            ui.AddToQueue("2,0 2,1 2,2 2,3");
 
             game.Run();
 
             Assert.Equal(expectedOutput, ui.LastString);
         }
-
-        
+ 
     }
+
+    
     public class StubUI : IUserInterface
     {
         public string LastString{get; private set;}
